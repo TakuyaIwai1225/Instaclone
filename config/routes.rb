9e05d93a4f4d291848c032569bee0fd3f727ae71
TakuyaIwai <tha1225.1010@gmail.com>
2018-05-27 23:33:12 +0000

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :blogs do
     collection do
       post :confirm
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites, only: [:create, :destroy]
-    end
-  end
   if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
