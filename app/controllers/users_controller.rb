5823efdfa28fb84f8ca99a404cbe1578bc95fb2a
TakuyaIwai <tha1225.1010@gmail.com>
2018-06-03 01:32:52 +0000

    finish Uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile upadated!"
+      redirect_to @user
+    else
+      render 'edit'
+      flash[:danger] = "errors"
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:image,:image_cache)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602111415) do
+ActiveRecord::Schema.define(version: 20180603005219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180602111415) do
     t.datetime "updated_at", null: false
     t.bigint "blog_id"
     t.string "picture"
+    t.string "image"
     t.index ["blog_id"], name: "index_users_on_blog_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
