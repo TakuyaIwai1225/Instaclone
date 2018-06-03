b898ef363d292a679e6b7b82e989f3b83f87f2d9
TakuyaIwai <tha1225.1010@gmail.com>
2018-06-03 02:27:32 +0000

    modified

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,7 +21,6 @@ class BlogsController < ApplicationController
     @blog.image.retrieve_from_cache!  params[:cache][:image]
     @blog.user_id = current_user.id
     if @blog.save
-      ContactMailer.contact_mail(@blog).deliver 
       redirect_to blogs_path, notice: "ブログを作成しました！"
     else
       render'new'
